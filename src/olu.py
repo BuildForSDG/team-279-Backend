8515c12366485487f895c02dc9fd925ee3414cfe
Ebenezer Olumide Ayeni <olu@timmalholdings.co.za>
2020-05-12 22:17:40 +0200

    New commit

--- src/olu.py
+++ src/olu.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
 
-# init app
+# init ap
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
