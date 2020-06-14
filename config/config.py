e0b952e4076c45b1f6bd396bbd9e9e5668188a5e
boratonaj <boraton2010@gmail.com>
2020-06-14 16:45:58 +0200

    code clea up and refactor. coverage refactor

--- config/config.py
+++ config/config.py
@@ -12,10 +12,9 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    # if os.getenv("ENVIRONMENT") == "development":
-    #     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or 'sqlite:///' + os.path.join(basedir, 'vtender.sqlite')
-    # else:
     if os.getenv("ENVIRONMENT") == "development":
+        SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or 'sqlite:///' + os.path.join(basedir, 'vtender.sqlite')
+    else:
         SQLALCHEMY_DATABASE_URI = 'postgresql:///vtender'
         # SQLALCHEMY_DATABASE_URI = "postgres://lkryctgiwiqcum:180df7d87c07048e60d733f2803f6dca755b929e132289c2ba07a1c8df19fbdf@ec2-34-193-117-204.compute-1.amazonaws.com:5432/d2khvjku62o1bc"
 
